37fe180e7cf0bea98285b7fac57da3fd3547ad69
Neev Agarwal <agarwalneev2005@gmail.com>
2024-12-23 22:47:09 +0530

    Uber Completed

--- Microserices/user/routes/userRoutes.js
+++ Microserices/user/routes/userRoutes.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('../Controller/userController')
+const express = require('express');
+const router = express.Router();
+const userController = require('../Controller/userController');
+const authMiddleware = require('../Middleware/authmiddleware');
 
-router.post('/register' , userController.register)
+    
 
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/logout', userController.logout);
+router.get('/profile', authMiddleware.userAuth, userController.profile);
+router.get('/accepted-ride',authMiddleware.userAuth, userController.acceptedRide);
 
-module.exports = router
+module.exports = router;
